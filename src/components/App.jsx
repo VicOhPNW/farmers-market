c3850ffc2b20a8056eedd1d35a9cd26500160abb
Victoria Oh <ohvictori@gmail.com>
2018-11-14 15:53:06 -0800

    style background image to size as cover.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ function App(){
       <style jsx global>{`
             body {
                 background-image: url('https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-0.3.5&s=130bd6edc8aba4019a8245472d106ebb&auto=format&fit=crop&w=1950&q=80');
+                background-size:cover;
             }
         `}</style>
     </div>
