fc643dd10ebdfa64547c422b9e06b1a68504f3b1
Victoria Oh <ohvictori@gmail.com>
2018-11-14 15:42:07 -0800

    Add global style for background image.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-
 function App(){
   return (
     <div>
+      <style jsx global>{`
+            body {
+                background-image: url('https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-0.3.5&s=130bd6edc8aba4019a8245472d106ebb&auto=format&fit=crop&w=1950&q=80');
+            }
+        `}</style>
     </div>
   );
 }
